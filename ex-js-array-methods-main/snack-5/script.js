c538f35af71bbcc9b16c1a59222226ee0b7b45ab
martinarrose <mmartinarrose@gmail.com>
2026-04-08 10:57:25 +0200

    snack 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,9 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+const evenNums = [];
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 2 === 0) evenNums.push(nums[i]);
+}
+console.log(evenNums);
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
