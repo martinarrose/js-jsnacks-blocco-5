a9d978a1e7286d668986eeea110030aafd03fd43
martinarrose <mmartinarrose@gmail.com>
2026-04-08 10:02:42 +0200

    init, ex 1 & 2

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,7 @@
+const names = ["Edoardo", "Simone", "Francesco"];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
---
+++ ex-js-array-methods-main/snack-2/script.js
@@ -0,0 +1,12 @@
+const people = [
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
+];
+for (let i = 0; i < people.length; i++) {
+  const peopleName = people[i].name;
+  console.log(peopleName);
+}
+
+// Stampa in console tutti i nomi
+// Risultato: 'Paolo', 'Giulia', 'Marco'
